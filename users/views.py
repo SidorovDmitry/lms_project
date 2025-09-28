3544cd692d83dd8d0e17b6f815120cbb274b2f2e
Dmitri_Sidorov <kin19861@rambler.ru>
2025-09-28 17:29:19 +0300

    add PaymentViewSet

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Payment
+from .serializers import PaymentSerializer
+from .filters import PaymentFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
-# Create your views here.
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = PaymentFilter
+    ordering_fields = ['payment_date']
+    ordering = ['-payment_date']
+
+
