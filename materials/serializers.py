33d6d69c57a43e179a79609f03f95b0c7dc91c93
Dmitri_Sidorov <kin19861@rambler.ru>
2025-10-01 21:12:59 +0300

    correct serializers.py

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,12 +3,9 @@ from .models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор для модели Lesson.
-
+    """Сериализатор для модели Lesson.
     Предоставляет данные об отдельном уроке, включая его связь с курсом.
-    Поле 'course' содержит ID связанного курса (только для чтения по умолчанию).
-    """
+    Поле 'course' содержит ID связанного курса (только для чтения по умолчанию)."""
 
     class Meta:
         model = Lesson
@@ -19,8 +16,7 @@ class CourseSerializer(serializers.ModelSerializer):
     """
     Сериализатор для модели Course.
 
-    Включает вложенные данные об уроках (через LessonSerializer) и общее количество уроков
-    в курсе (поле lessons_count, вычисляется динамически).
+
     """
 
     lessons = LessonSerializer(many=True, read_only=True)
@@ -31,13 +27,6 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'description', 'preview', 'owner', 'lessons', 'lessons_count']
 
     def get_lessons_count(self, obj):
-        """
-        Возвращает количество уроков, связанных с данным курсом.
+        """Возвращает количество уроков, связанных с данным курсом."""
 
-        Args:
-            obj (Course): Экземпляр модели Course.
-
-        Returns:
-            int: Количество связанных уроков.
-        """
         return obj.lessons.count()
