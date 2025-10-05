31a935daecf2bf09d248708a19a2e6224399b9c2
Dmitri_Sidorov <kin19861@rambler.ru>
2025-10-05 20:49:11 +0300

    add serializers.py/get_is_subscribed

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Course, Lesson
+from .models import Course, Lesson, Subscription
+from .validators import validate_youtube_url
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -7,24 +8,30 @@ class LessonSerializer(serializers.ModelSerializer):
     Предоставляет данные об отдельном уроке, включая его связь с курсом.
     Поле 'course' содержит ID связанного курса (только для чтения по умолчанию)."""
 
+    video_url = serializers.URLField(validators=[validate_youtube_url])
+
     class Meta:
         model = Lesson
         fields = ['id', 'title', 'description', 'preview', 'video_url', 'course']
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор для модели Course.
+    """ Сериализатор для модели Course. """
 
-
-    """
-
-    lessons = LessonSerializer(many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
     lessons_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description', 'preview', 'owner', 'lessons', 'lessons_count']
+        fields = ['id', 'title', 'description', 'preview', 'owner', 'is_subscribed', 'lessons_count']
+
+    def get_is_subscribed(self, obj):
+        """ Определяет, подписан ли текущий пользователь на данный курс."""
+
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return Subscription.objects.filter(user=request.user, course=obj).exists()
+        return False
 
     def get_lessons_count(self, obj):
         """Возвращает количество уроков, связанных с данным курсом."""
