a05a153ffbf41abfde57ef64fdea64965f693c8f
Dmitri_Sidorov <kin19861@rambler.ru>
2025-10-12 23:17:56 +0300

    correct path and settings

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework',
     'django_filters',
+    'drf_spectacular',
+
     'users',
     'materials',
 
@@ -80,6 +82,16 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',  # Поддержка фильтрации через django-filter
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    ,
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'LMS Project API',
+    'DESCRIPTION': 'Платформа управления курсами и уроками',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_SETTINGS': {'deepLinking': True},
 }
 
 # Настройки срока действия токенов
@@ -138,3 +150,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+
+
+
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework.routers import DefaultRouter
-from users.views import PaymentViewSet
+from payments.views import CreatePaymentAPIView
+from users.views import PaymentViewSet, RegisterView, MyTokenObtainPairView
 
 router = DefaultRouter()
 router.register(r'payments', PaymentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Авторизация и регистрация
+    path('api/users/register/', RegisterView.as_view(), name='register'),
+    path('api/users/login/', MyTokenObtainPairView.as_view(), name='login'),
+
+    # Маршруты материалов
     path('api/materials/', include('materials.urls')),
-    path('api/payments/', include(router.urls))
+
+    # Платежи
+    path('api/payments/', include(router.urls)),
+
+    # Документация через drf-spectacular
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/payments/course/<int:course_id>/', CreatePaymentAPIView.as_view(), name='create-payment'),
+    path('courses/<int:course_id>/pay/', CreatePaymentAPIView.as_view(), name='course-pay'),
 ]
