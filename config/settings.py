b3f64bc852dc39790296aa8249be4ce1ef6a9a24
Dmitri_Sidorov <kin19861@rambler.ru>
2025-10-21 09:41:15 +0300

    correct settings.py

--- config/settings.py
+++ config/settings.py
@@ -83,8 +83,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',  # Поддержка фильтрации через django-filter
     ],
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
-    ,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SPECTACULAR_SETTINGS = {
